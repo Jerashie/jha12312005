cda90bf1cce277441b3e594af0a61797cc1013a0
hakkem <132269759+Jerashie@users.noreply.github.com>
2024-10-05 15:04:09 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+function openContact(url) {
+    window.open(url, '_blank');
+   }
+           document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+               anchor.addEventListener('click', function (e) {
+                   e.preventDefault();
+                   document.querySelector(this.getAttribute('href')).scrollIntoView({
+                       behavior: 'smooth'
+                   });
+               });
+           });
